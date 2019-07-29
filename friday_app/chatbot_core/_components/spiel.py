73ba31852ba478edb19c121de0add90aa6283b8c
Kim Clarence Penaflor <mws.ail@meralco.com.ph>
2019-07-30 07:17:22 +0800

    add view_parser

--- friday_app/chatbot_core/_components/spiel.py
+++ friday_app/chatbot_core/_components/spiel.py
@@ -7,7 +7,6 @@ Documented via reST
 Load and generate chatbot spiels
 """
 
-
 import re
 import json
 
@@ -19,6 +18,14 @@ DISPLAY_DIR = 'friday_app/chatbot_core/_components/content/'
 # Load Display Json
 def load_display(userID):
   global DISPLAY_DIR
+  """
+  Load Display Json
+  :param userID: Facebook User ID
+  :type userID: string
+  :returns: display json object
+  :rtype: json (dictionary)
+  """
+
   display = json.loads(open(DISPLAY_DIR+'displays.json').read())
   return display
 
@@ -64,8 +71,46 @@ def display_attachment(url, ftype):
   }]
 
 
+# Free text
+def free_text(text, delay):
+  """ 
+  Generate free text return blueprint
+
+  :type text: string
+  :param text: text message to send
+
+  :type delay: integer
+  :param delay: message sending action delay
+
+  :returns: Json response blueprint
+  :rtype: Json array
+  """
+  return [{
+    'type' : 'text',
+    'data' : text,
+    'options' : [],
+    'prefix' : '',
+    'delay' : delay
+  }]
+
+
 # Quick Reply Response
 def quick_reply(userID, displayID, options):
+  """
+  Generate FB quick response blueprint
+
+  :type userID: string
+  :param userID: Facebook User ID
+
+  :type displayID: string
+  :param displayID: json display ID
+
+  :type options: Json (dictionary) array
+  :param options: Quick Reply Buttons
+
+  :returns: Json response blueprint
+  :rtype: Json (dictionary) array
+  """
   display = get_display(userID, displayID)
   buttons = []
   for option in options:
@@ -84,6 +129,17 @@ def quick_reply(userID, displayID, options):
 
 # Generic Button Template Response
 def btn_menu(text, options):
+  """ 
+  Generate FB Button Template response blueprint
+  :type text: string
+  :param text: Button menu header
+
+  :type options: Json (dictionary) array
+  :param options: array of options
+
+  :returns: Json response blueprint
+  :rtype: Json array
+  """
   return [{
     'type' : 'btn',
     'data' : text,
--- friday_app/chatbot_core/_modules/e2e/view_parser.py
+++ friday_app/chatbot_core/_modules/e2e/view_parser.py
@@ -6,7 +6,6 @@ Documented via reST
 Parse user view from component spiels
 """
 
-
 import re
 import json
 from friday_app.chatbot_core._components import spiel
@@ -26,9 +25,145 @@ def get_view(sessionName):
 # Execute view parser and unit output
 # TODO View Parser Exe
 def exe(userID, sessionName, userInput):
-  pass
+  choices = None
+  inputValid = True
+  matchedOption = None
+  maxRetry = False
 
+  responses = []
+  altResponses = []
 
+  view = get_view(sessionName)
 
+  altDisplay = spiel.get_display(userID, ".option-notmatch")
+  if( len(view['opions']) == 0 and len(view['content']) > 0 ):
+    displayID = view['content'][-1]
+    display = spiel.get_display(userID, displayID)
+    altResponse = [{
+      'type' : view['optionType'],
+      'data' : display['data'],
+      'options' : [],
+      'prefix' : '',
+      'delay' : display['delay']
+    }]
 
+  else:
+    #TODO default spiels file json
+    altResponse = spiel.text(userID, ".fallback")
+    altResponse += [{
+      'type' : view['optionType'],
+      'data' : altDisplay['data'],
+      'options' : view['options'],
+      'prefix' : '',
+      'delay' : altDisplay['delay']
+    }]
 
+  if( userInput != None ):
+    if('any' not in view['acceptTypes'] ):
+      if( userInput['type'] not in view['acceptTypes'] ):
+        inputValid = False
+
+    if( len(view['options']) > 0 and inputValid):
+      inputData = None
+      if( userInput['type'] == 'payload' ):
+        inputData = userInput['data']['payload'].lower()
+
+      if( userInput['type'] == 'text' ):
+        inputData = userInput['data']['text'].lower()
+
+      if( userInput['type'] == 'attachment' ):
+        inputData = userInput['data']['urls'][0]['payload']['url'].lower()
+
+      optionPos = 0
+      if( view['optionType'] == 'quick_reply' or view['optionType'] == 'btn' ):
+        for option in view['options']:
+          for syn in option['options']:
+            if( inputData == syn or ' ' + syn + ' ' in inputData ):
+              matchedOption = optionPos
+          
+          if( matchedOption != None):
+            break
+          optionPos += 1
+
+      if( matchedOption == None ):
+        inputValid = False
+
+    errorLog = int(convo_data.get_item(userID, 'errorLog')) + 1
+    if( view['retries'] ):
+      if( errorLog >= view['retries'] ):
+        maxRetry = True
+    
+  contents = view['content']
+  lastContent = None
+  if( len(contents) > 1 and len(view['options']) > 0 ):
+    lastContent = contents[-1]
+    contents = contents[:len(contents)-1]
+    for displayID in contents:
+      display = spiel.get_display(userID, displayID)
+      responses.append({
+        'type' : 'text',
+        'data' : display['data'],
+        'options' : [],
+        'prefix' : '',
+        'delay' : display['delay']
+      })
+
+  if( view['optionType'] == 'btn' ):
+    for option in view['options']:
+      if( option['type'] == 'web_url' ):
+        matches = re.findall(r"\{{(.*?)\}}", option['url'])
+        if( len(matches) > 0 ):
+          for match in matches:
+            new_val = convo_data.get_item(userID, match)
+            option['url'] = option['url'].replace('{{'+match+'}}', new_val)
+
+  if( len(view['options']) == 0 ):
+    for displayID in contents:
+      display = spiel.get_display(userID, displayID)
+      responses.append({
+        'type' : 'text',
+        'data' : display['data'],
+        'options' : [],
+        'prefix' : '',
+        'delay' : display['delay']
+      })
+
+  elif( len(contents) == 1 and lastContent != None ):
+    choices = view['options']
+    display = spiel.get_display(userID, lastContent)
+    responses.append({
+      'type' : view['optionType'],
+      'data' : display['data'],
+      'options' : view['options'],
+      'prefix' : '',
+      'delay' : display['display']
+    })
+
+  elif( len(contents) == 1 ):
+    choices = view['options']
+    display = spiel.get_display(userID, contents[0])
+    responses.append({
+      'type' : view['optionType'],
+      'data' : display['data'],
+      'options' : view['options'],
+      'prefix' : '',
+      'delay' : display['delay']
+    })
+
+  else:
+    choices = view['options']
+    display = spiel.get_display(userID, lastContent)
+    responses.append({
+      'type' : view['optionType'],
+      'data' : display['data'],
+      'options' : view['options'],
+      'prefix' : '',
+      'delay' : display['delay']
+    })
+
+  if( config.environment == 'dev' ):
+    state = '[STATE]: ' + sessionName
+    responses = spiel.free_text(state, 0) + responses
+
+  return responses, altResponse, choices, matchedOption, inputValid, maxRetry
+  

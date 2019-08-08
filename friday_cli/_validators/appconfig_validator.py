1bcc183ea4c631a01eea0c9eb4a43c9c1023c28b
Clientrace <pkimclarence@yahoo.com.ph>
2019-08-08 09:24:58 +0800

    add recursive attribute checking

--- friday_cli/_validators/appconfig_validator.py
+++ friday_cli/_validators/appconfig_validator.py
@@ -14,43 +14,51 @@ class AppConfigValidator:
   """
 
   # App config file validation rule:
-  validationRule = {
-    'app:name' : {
-      'type' : 'string',
-      'value' : {
-        'set' : 'custom',
-        'type' : 'any'
-      }
-    },
-    'app:version' : {
-      'type' : 'string',
-      'value' : {
-        'set' : 'custom',
-        'type' : 'numeric'
-      }
-    },
-    'app:description' : {
-      'type' : 'string',
-      'value' : {
-        'set' : 'custom',
-        'type' : 'any'
-      }
-    },
-    'runtime' : {
-      'type' : 'string',
-      'value' : {
-        'set' : 'predefined',
-        'type' : 'string',
-        'lists' : ['python','go']
-      }
-    }
-  }
+  validationRule = json.loads(open('friday_cli/_validators/rules/appconfig_rule.json').read())
 
+  def __init__(self, appConfig):
+    """
+    Initialize App Configuration
+    :param appConfig: application config json
+    :param type: dictionary
+    """
+    self.appConfig = appConfig
 
-  def validate(self, appConfig):
+  @staticmethod
+  def _compare_keys(parent, config, dictRule):
+    for val in dictRule.keys():
+      if( val not in config.keys()):
+        return False
+    for val in dictRule.keys():
+      if( '_rule' not in dictRule[val] ):
+        return AppConfigValidator._compare_keys(val, config[val], dictRule[val])
+    return True
+
+  def attrib_check(self):
+    """
+    Check if app config is complete
+    :returns: returns whether the app configuration is complete
+    :rtype: boolean
+    """
+    result = AppConfigValidator._compare_keys('root', self.appConfig, AppConfigValidator.validationRule)
+    return result
+
+    return True
+    # for val in AppConfigValidator.validationRule.keys():
+    #   if( val not in self.appConfig ):
+    #     return False
+      
+    # return True
+
+  def type_check(self):
     """
     """
+    # for val in AppConfigValidator.validationRule.keys():
+    #   if( self.appConfig[val]['type'] )
     pass
+
+
+
 
 
 
--- friday_cli/tests/test_appconfig_validator.py
+++ friday_cli/tests/test_appconfig_validator.py
@@ -6,13 +6,38 @@ version 0.0.1
 App config validator Test
 """
 
+import json
 import unittest
+from friday_cli._validators.appconfig_validator import AppConfigValidator
 
 
-class AppConfigValidator(unittest.TestCase):
+class AppConfigValidatorTest(unittest.TestCase):
   """
   Friday Application Config File unit test
   """
+
+  # Test Application Configuration
+  appConfig = json.loads(open('friday_cli/tests/testconfig/appconfig.json').read())
+
+  def test_attrib_check(self):
+    """
+    Test attrib_check
+    """
+    appconfigValidator = AppConfigValidator(self.appConfig)
+    self.assertTrue(appconfigValidator.attrib_check())
+
+
+  @unittest.skip('test')
+  def test_type_check(self):
+    """
+    Test type_check
+    """
+    appconfigValidator = AppConfigValidator(self.appConfig)
+    appconfigValidator.type_check()
+
+
+
+
 
 
 

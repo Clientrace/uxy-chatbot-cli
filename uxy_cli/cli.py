58cb2e605bff49514913be6c254d7855400187cb
Kim Clarence Penaflor <pkimclarence@yahoo.com.ph>
2019-08-09 16:27:09 +0800

    add requirements.txt

---
+++ uxy_cli/cli.py
@@ -0,0 +1,29 @@
+"""
+Authored by Kim Clarence Penaflor
+08/09//2019
+version 0.0.2
+Documented via reST
+
+Project uxy cli command manager module
+"""
+
+
+
+import click
+from uxy_cli._generators.aws_setup import AWSSetup
+
+
+# TODO: Project Setup
+def setup():
+  pass
+
+# TODO: Uxy Chatbot Component generator
+def generate_component():
+  pass
+
+
+if __name__ == '__main__':
+  pass
+
+
+
